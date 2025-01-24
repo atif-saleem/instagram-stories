e1caecea39902cfd0637ff497f9c11878410b4ef
atif-saleem <atifsaleem0008@gmail.com>
2025-01-24 11:27:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var arr = [
+  {
+    dp: "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9kZWx8ZW58MHx8MHx8fDA%3D",
+    story:
+      "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bW9kZWx8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1526510747491-58f928ec870f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vZGVsfGVufDB8fDB8fHww",
+    story:
+      "https://images.unsplash.com/photo-1521577352947-9bb58764b69a?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fG1vZGVsfGVufDB8fDB8fHww",
+    story:
+      "https://images.unsplash.com/photo-1563993297290-609c9406efcd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fG1vZGVsfGVufDB8fDB8fHww",
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fG1vZGVsfGVufDB8fDB8fHww",
+    story:
+      "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fG1vZGVsfGVufDB8fDB8fHww",
+  },
+];
+var storyian = document.querySelector(".storyian");
+var fullScreen = document.querySelector(".full-screen");
+var clutter = "";
+arr.forEach((elem, indx) => {
+  clutter += `<div class="story">
+    <img id="${indx}" src="${elem.dp}" alt="" />
+  </div>`;
+});
+storyian.innerHTML = clutter;
+storyian.addEventListener("click", (dets) => {
+  fullScreen.style.display = "block";
+  fullScreen.style.backgroundImage = `url(${arr[dets.target.id].story}`;
+  setTimeout(() => {
+    fullScreen.style.display = "none";
+  }, 3000);
+});
